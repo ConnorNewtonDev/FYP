c26856749a1e30d99339dd4c5e427299c922a38b
ConnorNewtonDev <dev@connornewton.co.uk>
2019-04-16 17:31:53 +0100

    Block Receiver

---
+++ Chambers/Assets/Scripts/Challenge Managers/CubeTarget.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Interactable")
+        {
+
+            CheckPosition(other.transform.position);
+            CheckPosition(other.transform.position, 5.0f);
+        }
+    }
+
+    private bool CheckPosition(Vector3 position, float variance = 0.5f)
+    {
+        if(Vector3.Distance(this.transform.position, position) <= variance)
+            return true;
+        else 
+            return false;
+    }
+
+    private IEnumerator ReceiveCube()
+    {
+
+
+        yield return null;
+    }
+}
